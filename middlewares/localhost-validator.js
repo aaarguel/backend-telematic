401800869c5188ccc4eeb01dd6e15030f0d3f9ce
Aaron Arguello <aaarguel@espol.edu.ec>
2022-08-29 02:32:05 -0500

    Fixes bools

--- middlewares/localhost-validator.js
+++ middlewares/localhost-validator.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 
 const validateLocalhost = ( req, res= response, next ) => {   
     console.log(req._remoteAddress);
-    if( req._remoteAddress != "::1" || !req._remoteAddress.includes("127.0.0.1")){
+    if( req._remoteAddress != "::1" && !req._remoteAddress.includes("127.0.0.1")){
      return res.status(400).json({
          ok: false,
          msg: "You don't have permissions to consume this route"
